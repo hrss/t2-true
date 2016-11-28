cba54bd1de2d5918dfaa5018453f63d912dad3e7
Joao Paulo Soubihe <ra151106@felix.lab.ic.unicamp.br>
2016-11-27 22:35:37 -0200

    passagem de parametros pela pilha

--- functions/set_motor_speed.s
+++ functions/set_motor_speed.s
@@ -1,108 +1,161 @@
 set_motor_speed:
 
-  stmfd sp!, {r4-r11, lr} @salvei registradores não utilizados
-  add fp, sp, #32
-  sub sp, #4
-  ldr r1, [fp,#4]         @velocidade
-  ldr r0, [fp,#8]         @id do motor
+  	mrs r0, CSPR
+  	orr r0, r0, #0b11111
+	msr CPSR, r0
 
-  cmp r0, #1              @averigua se a id do motor e valida
-  cmpne r0,#0
-  movne r0, #-1
-	mov r0, #0
+	mov r0, sp
+ 
 
-  movmi r1, #-2           @confere se a velocidade e negativa
+	mrs r0, CSPR
+	and r0, r0, #0b11111111111111111111111111110011
+	msr CPSR, r0
+
+	push {r1, r2}
+	ldmdb r0, {r1-r2}
+
+	mov r0, r1		@r0 e id
+	mov r1, r2		@r1 e velocidade
+		
+
+  
+  	cmp r0, #1              @averigua se a id do motor e valida
+  	cmpne r0,#0
+  	mov r0, #-1
+  
+							
+  	movmi r1, #-2           @confere se a velocidade e negativa
 
 
-  cmp r1, #63             @confere se o valor da velocidade valesse mais que um bit
-  movgt r1, #-2
-	mov r0, #0	
+  	cmp r1, #63             @confere se o valor da velocidade valesse mais que um bit
+  	movgt r1, #-2
+  
 	
+  	cmp r0, #0		@se a cadeia de lacos rolar a funcao chama o hardware se tudo ok
+  	beq write_motor_0
+		
+  	cmp r0, #1
+  	beq write_motor_1	@r0 id e r1 velocidade					
 	
-  lmfd sp!, {r4-r11,lr}		@desempilha os registradores não utilizados
-  movs pc,lr
+  	pop {r1-r2}
+
+  	movs pc,lr
 
 
 set_motors_speed:	
 
-	stmfd sp!, {r4-r11, lr} @salvei registradores não utilizados
-  add fp, sp, #32
-  sub sp, #4
-  ldr r1, [fp,#4]         @velocidade de 1
-  ldr r0, [fp,#8]         @velocidade de 0
+	mrs r0, CSPR
+	orr r0, r0, #0b11111
+	msr CPSR, r0							
+	
+	mov r0, sp
+ 
 
+	mrs r0, CSPR
+	and r0, r0, #0b11111111111111111111111111110011
+	msr CPSR, r0
+	
+	push {r1, r2}
+	ldmdb r0, {r1-r2}
+
+	mov r0, r1
+	mov r1, r2
+									
 	mov r2, #0
-	mov r3, #1
+	mov r3, r1							@velocidade 1 esta em r3
 
-	mov r4, r0
-	mov r0, r2
-	mov r5, r1
-	mov r1, r4
-
+	mov r1, r0
+	mov r0, r2							@ok para motor 0	
+	
 	mov r7, #18							@chama a syscall set_motor_speed para o motor 0
 	svc 0x0
 
-	cmp r0, #-2
+	cmp r0, #-2							@confere se o retorno de set_motor_speed e valido
 	moveq r0, #-1
 	
-	mov r2, r0
-	mov r0, r3
-	mov r1, r5
+	mov r1, r3
+	mov r3, r0							@r3 fica com r0(velocidade do motor 0)
+
+	mov r0, #1
 
 	svc 0x0									@chama a syscall set_motor_speed para o motor 1
 	
 	cmp r0, #-2
 	moveq r0, #-2
 
-	mov r3, r0
-	mov r0, r2							@ajeita os parametros para retorno da funcao
-	mov r1, r3
-
-	lmfd sp!, {r4-r11, lr}
-
+	mov r1, r0
+	mov r0, r3
 	
+	pop {r1-r2}
+
 	movs pc,lr
 
 
 read_sonar:
-
-	stmfd sp!, {r4-r11, lr} @salvei registradores não utilizados
-	add fp, sp, #32
-	sub sp, #4
-	ldr r0, [fp, #4]
 	
-	cmp r0, #0
-	movlt r0, #-1	
+	mrs r0, CSPR
+	orr r0, r0, #0b11111
+	msr CPSR, r0							
+	
+	mov r0, sp
+ 
+
+	mrs r0, CSPR
+	and r0, r0, #0b11111111111111111111111111110011
+	msr CPSR, r0
+
+	push {r1}
+	ldmdb r0, {r1}
+
+	mov r0, r1
+
+	
+	movmi r0, #-1	
 
 	cmp r0, #15
 	movgt r0, #-1
 	
-	lmfd sp!, {r4-r11, lr}
-
+	cmp r0, #-1
+	bne read_sonar_with_id	
+	
+	pop {r1}	
+	
 	movs pc, lr
 	
-	
+
 	
 
 	
 get_time:
 
+	rotuloRETORNA_TEMPO	
 	
 	movs pc, lr
 
 
 set_time:
-
-
-	stmfd sp!, {r4-r11, lr} @salvei registradores não utilizados
-	add fp, sp, #32
-	sub sp, #4
-	ldr r0, [fp, #4]
-	lmfd sp!, {r4-r11, lr}
+	
 	
 
-	set_alarm:
-
-
+	
+	
+	
+	
 	
 
+set_alarm:
+	
+	ldr r2,=MAX_ALARMS
+	ldr r2, [r2]
+
+	bl get_time
+
+	cmp r1,r0
+	movlt r0, #-2 			@se tempo menor que tempo do sistema r0 = -2
+	mov r0, #0					@senão r0 = 0
+	
+
+	
+	
+
+	
