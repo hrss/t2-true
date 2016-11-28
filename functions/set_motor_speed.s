a5e538dbedb79fcc553f7872a58f5402be09057c
Joao Paulo Soubihe <ra151106@felix.lab.ic.unicamp.br>
2016-11-27 23:44:48 -0200

    funcoes zikas feitas

--- functions/set_motor_speed.s
+++ functions/set_motor_speed.s
@@ -110,10 +110,9 @@ read_sonar:
 	mov r0, r1
 
 	
-	movmi r0, #-1	
-
+	
 	cmp r0, #15
-	movgt r0, #-1
+	movhi r0, #-1
 	
 	cmp r0, #-1
 	bne read_sonar_with_id	
@@ -161,17 +160,112 @@ set_time:
 
 set_alarm:
 	
-	ldr r2,=MAX_ALARMS
+	mrs r0, CSPR
+  	orr r0, r0, #0b11111
+	msr CPSR, r0	
+
+	mov r0, sp
+ 
+
+	mrs r0, CSPR
+	and r0, r0, #0b11111111111111111111111111110011
+	msr CPSR, r0
+
+	push {r1-r3}
+	ldmdb r0, {r1-r2}
+
+	mov r0, r1		@r0 e ponteiro
+	mov r1, r2		@r1 e tempo do alarme
+
+	ldr r2,=CONTADOR
 	ldr r2, [r2]
 
-	bl get_time
+	cmp r2, r1
+	movhi r0, #-2
 
-	cmp r1,r0
-	movlt r0, #-2 			@se tempo menor que tempo do sistema r0 = -2
-	mov r0, #0					@senão r0 = 0
-	
+	movshi pc, lr
 
 	
+	ldr r2,=ALARMS
+	ldr r2, [r2]
 	
+	cmp r2, #MAX_ALARMS
+	mov r0, #-1
+	movseq pc, lr
+
+	ldr r3, =ALARMS_FUNCTION_BASE	@vetor de funcoes de alarme
+	mul r2, r2, #4			@posicao correta do ponteiro(respectiva funcao)
+	str r0, [r3, r2]		
+	
+	ldr r3, =ALARMS_TIME_BASE
+	str r1, [r3, r2]
+
+	ldr r2, =ALARMS
+	ldr r3, [r2]
+	add r3, r3, #1
+	str r3, [r2]	
+
+	pop {r1-r3}
+	
+	mov r0, #0
+
+	movs pc, lr
+	
+	
+register_proximity_callback:
+
+	mrs r0, CSPR
+  	orr r0, r0, #0b11111
+	msr CPSR, r0	
+
+	mov r0, sp
+ 
+
+	mrs r0, CSPR
+	and r0, r0, #0b11111111111111111111111111110011
+	msr CPSR, r0
+
+	push {r1-r4}
+	ldmdb r0, {r1-r3}
+
+	mov r0, r1			@r0 e sonar_id
+	mov r1, r2			@r1 e limiar
+	mov r2, r3			@r2 e ponteiro da funcao
+
+
+	cmp r0, #15
+	movhi r0, #-2
+
+	movshi pc, lr
+
+
+	ldr r3, =CALLBACKS
+	ldr r3, [r3]
+
+	cmp r3, #MAX_CALLBACKS 
+	movhi r0, #-1
+	
+	
+	movshi pc, lr
+
+	ldr r4, =CALLBACKS_SONAR_BASE	@vetor de sonares
+	mul r3, r3, #4			@posicao correta do sonar
+	str r0, [r4, r3]		
+	
+	ldr r4, =CALLBACK_THRESHOLD_BASE	@guarda as limiares de cada callback
+	str r1, [r4, r3]
+	
+	ldr r4, =CALLBACK_FUNCTION_BASE	@guarda as limiares de cada callback
+	str r2, [r4, r3]
+
+	ldr r3, =CALLBACKS		@incrementa o valor em CALLBACKS
+	ldr r4, [r3]
+	add r4, r4, #1
+	str r4, [r3]	
+
+	pop {r1-r4}
+	mov r0, #0
+
+	movs pc, lr
 
 	
